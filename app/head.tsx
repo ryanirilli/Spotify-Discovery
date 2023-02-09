f95926f9646c896ee162ab7ed9ea5d95acd18b96
Ryan Irilli <ryanirilli@gmail.com>
2023-02-09 13:51:11 -0800

    prevent zoom on input focus

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,10 @@ export default function Head() {
   return (
     <>
       <title>Spotworm</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      />
       <meta
         name="description"
         content="A better way to discover music on Spotify"
